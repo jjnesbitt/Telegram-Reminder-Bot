bd2f691914a79b1f3dbbfb69c49368f791d7be98
Jake <jjnesbitt2@gmail.com>
2018-08-11 06:44:23 +0000

    Cleaned some stuff up

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
 import fs from 'fs';
+import _ from 'lodash';
 
 import { BOT_TOKEN, BOT_USERNAME } from './credentials';
 
-let app = express();
+const app = express();
 
-process.exit(0);
-
-const MATCH_NOT_FOUND_TEXT = "Sorry, I don't know what you want."
+const MATCH_NOT_FOUND_TEXT = "Sorry, I don't know what you want.";
 const SSL_OPTIONS = {
     key: fs.readFileSync('./telegram_private.key'),
-    cert: fs.readFileSync('./telegram_public.pem')
+    cert: fs.readFileSync('./telegram_public.pem'),
 };
 
 //In milliseconds
@@ -25,18 +24,20 @@ const time_units = {
     day: 86400000,
     week: 604800000,
     month: 2419200000,
-    year: 29030400000
+    year: 29030400000,
 };
+
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: true,
 })); // for parsing application/x-www-form-urlencoded
 
 //Function called to forward message, usually set on a timer.
-function forwardMessage(params) {
+
+const forwardMessage = params => {
     axios.post('https://api.telegram.org/bot' + BOT_TOKEN + '/forwardMessage', params)
-        .then(response => {
+        .then(() => {
             // We get here if the message was successfully posted
             console.log('Message forwarded');
         })
@@ -44,12 +45,12 @@ function forwardMessage(params) {
             // ...and here if it was not
             console.log('Error :', err);
         });
-}
+};
 
 //Function called to send message, usually set on a timer.
 function sendMessage(params) {
     axios.post('https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage', params)
-        .then(response => {
+        .then(() => {
             // We get here if the message was successfully posted
             console.log('Message sent');
         })
@@ -60,77 +61,37 @@ function sendMessage(params) {
 }
 
 function findMatchFromText(text) {
-    const found = 'found';
+    const regex = new RegExp(`(\\d{1,10}(?:\\.\\d{0,10})?) (${_.keys(time_units).join('|')})`);
+    const match = regex.exec(text);
 
-    let returnObject = {};
+    if (!text || !match.length) return { found: false };
 
-    if (!text) {
-        returnObject[found] = false;
-    }
-    else {
-        let matches = [];
-        for (let key in time_units) {
-            //let regex = new RegExp(BOT_USERNAME + " (\\d{1,10}.?\\d{0,10}) " + key);
-            let regex = new RegExp("(\\d{1,10}.?\\d{0,10}) " + key);
-            let match;
-            if ((match = regex.exec(text)) != null) {
-                //Means valid number has been matched
+    const num = parseFloat(match[1]);
+    const unit = match[2];
+    const wait = num * time_units[unit];
 
-                let num = match[1];
-                matches.push(parseFloat(num));
-                matches.push(key);
-                break;
-            }
-        }
+    console.log("TTW: " + wait);
 
-        if (matches.length == 0) {
-            returnObject[found] = false;
-        }
-        else {
-            let num = matches[0];
-            let units = matches[matches.length - 1];
-            let timeToWait = num * time_units[units];
-
-            console.log("TTW: " + timeToWait);
-
-            //Format before return
-            if (num != 1) units += 's';
-
-            returnObject[found] = true;
-            returnObject['wait'] = timeToWait;
-            returnObject['num'] = num;
-            returnObject['units'] = units;
-        }
-    }
-
-    return returnObject;
+    return {
+        wait,
+        num,
+        found: true,
+        units: num === 1 ? unit : `${unit}s`,
+    };
 }
 
-function confirmReminderSet(chat_id, reply_to_message_id, numberOfUnits, units) {
-    axios.post('https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage', {
-        'chat_id': chat_id,
-        'text': ('Reminder set for ' + numberOfUnits + ' ' + units + ' from now.'),
-        'reply_to_message_id': reply_to_message_id
-    })
-        .then(response => {
-            // We get here if the message was successfully posted
-            console.log('Message posted')
-        })
-        .catch(err => {
-            // ...and here if it was not
-            console.log('Error :', err)
-        })
-}
-
-function checkIfFromTelegram(req){
-    let ip = req.connection.remoteAddress.split('::ffff:')[1];
-
-    console.log('REQUEST IP: ' + ip);
+function checkIfFromTelegram(req) {
     const ipToCheck = '149.154.167';
-    let splitIP = ip.split('.');
 
-    if (splitIP[0] + '.' + splitIP[1] + '.' + splitIP[2]  != ipToCheck) return false;
-    if (parseInt(splitIP[3]) < 197 || parseInt(splitIP[3]) > 233) return false;
+    // const ip = req.connection.remoteAddress.split('::ffff:')[1];
+    // const ipArray = ip.split('.');
+    const ipArray = req.connection.remoteAddress.split('::ffff:')[1].split('.');
+
+    console.log('REQUEST IP: ' + ipArray.join('.'));
+
+    // if (ipArray[0] + '.' + ipArray[1] + '.' + ipArray[2] != ipToCheck) return false;
+    if (_.dropRight(ipArray).join('.') != ipToCheck) return false;
+    if (parseInt(ipArray[3]) < 197 || parseInt(ipArray[3]) > 233) return false;
 
     return true;
 }
@@ -143,10 +104,10 @@ app.get('/', function (req, res) {
 });
 
 //This is the route the API will call
-app.post('/', function (req, res) {
+app.post('/', (req, res) => {
     res.end();
 
-    if (!checkIfFromTelegram(req)){
+    if (!checkIfFromTelegram(req)) {
         console.log('NOT FROM TELEGRAM, quitting...');
         return;
     }
@@ -158,22 +119,20 @@ app.post('/', function (req, res) {
         return;
     }
 
-    let sender_id = message.from.id;
-    let message_id = message.message_id;
-    let chat_id = message.chat.id;
+    const sender_id = message.from.id;
+    const message_id = message.message_id;
+    const chat_id = message.chat.id;
 
-    let match = findMatchFromText(message.text);
+    const match = findMatchFromText(message.text);
 
-    let messageFunction;
-    let params = {
+    const params = {
         chat_id: sender_id,
-        from_chat_id: chat_id
+        from_chat_id: chat_id,
     };
 
     let REPLY = false;
     let FORWARD = false;
 
-    messageFunction = forwardMessage;
     // params['from_chat_id'] = chat_id;
 
     if (message.chat.type == 'private') {
@@ -182,18 +141,18 @@ app.post('/', function (req, res) {
 
         console.log('private chat');
 
-        if (message.forward_from_message_id){
+        if (message.forward_from_message_id) {
             //Contains a forwarded message
             FORWARD = true;
-            params['message_id'] = message.forward_from_message_id;
+            params.message_id = message.forward_from_message_id;
         }
-        else if (message.reply_to_message){
+        else if (message.reply_to_message) {
             //Is a reply to a message
             REPLY = true;
-            params['message_id'] = message.reply_to_message.message_id;
+            params.message_id = message.reply_to_message.message_id;
         }
-        else{
-            params['message_id'] = message_id;
+        else {
+            params.message_id = message_id;
         }
     }
     else {
@@ -205,42 +164,46 @@ app.post('/', function (req, res) {
             if (message.reply_to_message) {
                 //Message is a reply to another message
                 REPLY = true;
-                params['message_id'] = message.reply_to_message.message_id;
+                params.message_id = message.reply_to_message.message_id;
             }
             else {
-                params['message_id'] = message_id;
+                params.message_id = message_id;
             }
         }
-        else{
+        else {
             //Bot not mentioned, ignore
             return;
         }
     }
 
     //We've gotten here, which means that the bot either had a message sent directly to it, or it has been mentioned publicly
-    if (match.found){
+    if (match.found) {
         //Either forward or send message
-        setTimeout(function(){messageFunction(params);}, match.wait);
+        setTimeout(() => { forwardMessage(params); }, match.wait);
 
         //Confirm to sender that reminder is set.
         let text = 'Reminder set for ' + match.num + ' ' + match.units + ' from now.';
         if (!REPLY && !FORWARD) text = "No message specified, " + text;
 
         sendMessage({
-            'chat_id': chat_id,
-            'reply_to_message_id': message_id,
-            'text': text
+            chat_id,
+            text,
+            reply_to_message_id: message_id,
         });
     }
-    else{
+    else {
         sendMessage({
-            'chat_id': chat_id,
-            'reply_to_message_id': message_id,
-            'text': MATCH_NOT_FOUND_TEXT
+            chat_id,
+            reply_to_message_id: message_id,
+            text: MATCH_NOT_FOUND_TEXT,
         });
     }
     return;
 });
+
+process.exit(0);
+
+// Must fix networking with nginx past here
 
 //create server to listen for api call
 https.createServer(SSL_OPTIONS, app).listen(443);
