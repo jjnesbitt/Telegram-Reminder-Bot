53e7155d49fd63238885d0fed6bc4ec6843f0d76
Jake <jjnesbitt2@gmail.com>
2018-11-07 11:33:47 -0500

    rmeove scripts

--- scripts/getWebhookInfo.sh
+++
@@ -1,1 +0,0 @@
-sudo curl https://api.telegram.org/bot459017535:AAHOlNDZ1lRwzpo_-34daWHuZpUXkQ8M4Ec/getWebhookInfo
--- scripts/set_webhook.sh
+++
@@ -1,1 +0,0 @@
-sudo curl -F "url=https://aafl8jubmkwhyga2aate7tlhbrcstxjfog0.jakenesbitt.com" "https://api.telegram.org/bot459017535:AAHOlNDZ1lRwzpo_-34daWHuZpUXkQ8M4Ec/setWebhook"
