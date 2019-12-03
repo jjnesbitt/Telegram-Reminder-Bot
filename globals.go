837f826782a544740c4d9a757925d395e9660457
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-12-03 01:57:38 -0500

    Remove old commented code

--- globals.go
+++ globals.go
@@ -38,16 +38,3 @@ func init() {
 	mongoURL = os.Getenv("MONGO_URL")
 	mongoPort = os.Getenv("MONGO_PORT")
 }
-
-// var dotenvErr = godotenv.Load()
-
-// var envBotToken = os.Getenv("BOT_TOKEN")
-// var envListenPort = os.Getenv("LISTEN_PORT")
-// var envRootPublicURL = os.Getenv("ROOT_PUBLIC_URL")
-// var envMode = os.Getenv("BOT_MODE")
-
-// var publicURL = envRootPublicURL + "/" + envBotToken
-
-// var rootTelegramMethodURL = "https://api.telegram.org/bot" + envBotToken
-
-// var currentLimboUsers map[int]*tb.Message = make(map[int]*tb.Message)
